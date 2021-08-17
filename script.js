c7c646b0f41819f095050ab978ea967215646e02
Kyle McWilliams <kmac52694@gmail.com>
2021-08-17 14:01:18 -0500

    5dayforecast working finally, jeeze!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// const currentDayEl = document.getElementById('currentDay')
-// var currentHour = moment().format("dddd, MMMM Do YYYY");
-// currentDayEl.textContent = currentHour;
+const currentDayEl = document.getElementById('currentDay')
+var currentHour = moment().format("dddd, MMMM Do YYYY");
+currentDayEl.textContent = currentHour;
 // why is moment undefined?
 
 
@@ -30,60 +30,49 @@ function todaysWeather(city) {
         return response.json();
       })
       .then(function (data) {
-
-        if(data.weather[0] >199 && data.weather[0] <233){
-          document.body.style.backgroundImage = "./thunderstorm.jpg"
-        } else if (data.weather[0] >300 && data.weather[0] <321){
+        console.log(data)
+        if(data.weather[0].id >199 && data.weather[0].id <233){
+          document.body.style.backgroundImage = "url(./thunderstorm.jpg)"
+        } else if (data.weather[0].id >300 && data.weather[0].id <321){
           document.body.style.backgroundImage = "./drizzle.jpg"
-        }else if (data.weather[0] >499 && data.weather[0] <522){
+        }else if (data.weather[0].id >499 && data.weather[0].id <522){
           document.body.style.backgroundImage = "./rain.jpg"
-        }else if (data.weather[0] >599 && data.weather[0] <623){
+        }else if (data.weather[0].id >599 && data.weather[0].id <623){
           document.body.style.backgroundImage = "./snow.jpg"
-        }else if (data.weather[0] >699 && data.weather[0] <782){
+        }else if (data.weather[0].id >699 && data.weather[0].id <782){
           document.body.style.backgroundImage = "./fog.jpg"
-        } else if (data.weather[0] >800 && data.weather[0] <805){
-          document.body.style.backgroundImage = "./cloudy.jpg"
-        }else if (data.weather[0] === 800){
+        } else if (data.weather[0].id >800 && data.weather[0].id <805){
+          document.body.style.backgroundImage = "url(./cloudy.jpg)"
+        }else if (data.weather[0].id === 800){
           document.body.style.backgroundImage = "./clear.jpg"
         }
-
-
-
-
-
-
-        // var currentCity = data.main.temp      where is city in the localStorage
-        // currentCityEl.innerHTML = currentCity       
+        
+         // how to make first letter captial?	
+        currentCityEl.innerHTML = data.name
+           
         var temp = data.main.temp
         tempEl.innerHTML = "Today's temperature is " + temp + " degrees"
         var humidity = data.main.humidity
         humidityEl.innerHTML = "Humidity is " + humidity + " %"
         var wind = data.wind.speed
         var windDegreeEl = data.wind.deg
-        windEl.innerHTML = "Wind speed is " + wind + " mph" //+ emojois         
-        var emojiEl = document.getElementById('emojis');
-        // emojiEl.src = 'https://openweathermap.org/img/w' + todaysWeather.weather[0].icon + '.png';       
+        var emojis = document.createElement("img");
+        var emojiEl = document.getElementById('emojis');        
+        emojis.src = 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png';           
+        windEl.innerHTML = "Wind speed is " + wind + " mph"          
 
-
+    if(windDegreeEl > 0 && windDegreeEl < 90){
+  windEl.append(' from the southwest');
+} else if (windDegreeEl >= 90 && windDegreeEl < 180){
+  windEl.append(" from the northwest");
+} else if (windDegreeEl >=180 && windDegreeEl < 270){
+  windEl.append(" from the northeast");
+} else if (windDegreeEl >=270 || windDegreeEl === 0 ) {
+  windEl.append(" from the south east")}
       });     
 }
 
-
-//how to append this to wind degree??
-// if(windDegreeEl > 0 && windDegreeEl < 90){
-//   innerHTML = "from the southwest";
-// } else if (windDegreeEl >= 90 && windDegreeEl < 180){
-//   innterHTML = "from the northwest";
-// } else if (windDegreeEl >=180 && windDegreeEl < 270){
-//   innerHTML = "from the northeast"
-// } else if (windDegreeEl >=270 || windDegreeEl === 0 ) {innerHTML = "from the south east"}
-
-
-
-
-
-
-
+const forecastEl = document.querySelector('.forecast')  
 
 function fiveDayForecast(city){
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`).then(function (response) {
@@ -92,20 +81,24 @@ function fiveDayForecast(city){
       .then(function (data) {
         for (let i = 0; i < data.list.length; i++) {
             const element = data.list[i];
-          // const forecastEl = document.querySelector('#forecast')  
-          //   var div = document.createElement("div");
-          //     div.style.width = "100px";
-          //     div.style.height = "100px";
-          //     div.style.textAlign = "center";
-          //     div.style.background = "blue";
-          //     div.style.color = "white";
-          //     div.innerHTML =  temp + " degrees";
-          //     div.innerHTML = humidity + " degrees";  //do i need to move them from the previous function into the global scale
-          //     div.innerHTML = wind + " mph";
-          // forecastEl.append(div)    
-
     if(element.dt_txt.indexOf('12:00:00') !== -1){
     console.log(element);
+            var div = document.createElement("div");
+              div.style.width = "100px";
+              div.style.height = "100px";
+              div.style.textAlign = "center";
+              div.style.background = "blue";
+              div.style.color = "white";
+              var pTag0 = document.createElement("p");
+              pTag0.innerHTML = new Date (element.dt_txt).toLocaleDateString();
+              var pTag = document.createElement("p");
+              pTag.innerHTML =  element.main.temp + " degrees";
+              var pTag2 = document.createElement("p");
+              pTag2.innerHTML = element.main.humidity + " %";  
+              var pTag3 = document.createElement("p");
+              pTag3.innerHTML = element.wind.speed + " mph";
+              div.append(pTag0 , pTag , pTag2 , pTag3)
+          forecastEl.append(div)    
 }
         }
       });
